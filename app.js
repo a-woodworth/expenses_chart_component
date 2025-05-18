2a29746a773981b9b39b02c0aec49343b805325e
Amy Woodworth <woodworthcommunications@gmail.com>
2025-05-18 10:17:51 -0500

    Fix js issue to highlight correct day.

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 function getDayOfWeek() {
   // Get the current date
   const today = new Date();
-  const dayOfWeek = today.getDay() - 1; // Adjust to 0-6 range
+  const dayOfWeek = today.getDay(); // Sunday - Saturday: 0 - 6
 
   // Get corresponding day from chart
-  const chartDays = document.querySelectorAll('.chart__table-day');
-  const dayArray = Array.from(chartDays);
-  const currentDayColumn = dayArray[dayOfWeek].nextElementSibling;
+  const chartDays = [
+    ...document.querySelectorAll('.chart__table-day'),
+  ];
+  let currentDayColumn;
+
+  // If it's Sunday, get the last chart column amount
+  if (dayOfWeek === 0) {
+    currentDayColumn = chartDays[6].nextElementSibling;
+  } else {
+    // Subtract 1 to get the correct chart column amount
+    currentDayColumn = chartDays[dayOfWeek].nextElementSibling;
+  }
 
   // Add class to highlight the current day
   currentDayColumn.classList.add('highlight');
